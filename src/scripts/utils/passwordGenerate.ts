1bf1041b28f23e54d27b9a6f1f02b61787a73aa3
patrickcunha <patrick.cunha336@gmail.com>
2018-12-09 03:53:06 -0300

    update util scripts

--- src/scripts/utils/passwordGenerate.ts
+++ src/scripts/utils/passwordGenerate.ts
@@ -4,16 +4,16 @@ const { argv } = process
 const { APP_SECRET } = process.env
 
 if (argv.length !== 3) {
-  throw new Error('Por favor passe a senha como argumento!')
+  throw new Error('Please, input the password as argument!')
 }
 
 if (!APP_SECRET || APP_SECRET === '') {
-  throw new Error('APP_SECRET não definido como variável de ambiente (env)')
+  throw new Error('APP_SECRET is not defined on the enviromnent file (.env)')
 }
 
 const senha:string = argv[2]
 
-console.log('Gerando hash...')
+console.log('Generate hash...')
 
 const hash:string = bcrypt.hashSync(senha + APP_SECRET)
 
